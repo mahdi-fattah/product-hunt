679a5e8c744ed1ad5962dfc2bcd6f3620c6a0270
Mahdi FattahPour <mahdi.fattah.1377@gmail.com>
2021-05-30 21:20:03 +0430

    Final commit

--- ProductHuntClone/settings.py
+++ ProductHuntClone/settings.py
@@ -126,7 +126,5 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
